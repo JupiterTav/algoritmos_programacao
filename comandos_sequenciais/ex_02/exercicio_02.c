65b1c6eadb2a9e75f05a01d14509ccc60edb7da8
JupiterTav <wesleytavaresdev@outlook.com>
2025-03-12 21:37:01 -0300

    v0.1

---
+++ comandos_sequenciais/ex_02/exercicio_02.c
@@ -0,0 +1,13 @@
+/*Dado o preço de um produto em reais, converter este valor para o equivalente
+em dólares. O programa deverá ler o preço e a taxa de conversão para o dólar.*/
+
+/*
+    ler (preço)
+    ler (taxaConversão)
+
+    preçoDolar <- conversão(preço, taxa)
+
+    imprimir preçoDolar
+*/
+
+int main() { return 0; }
